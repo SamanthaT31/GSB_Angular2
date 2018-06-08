e7b693326d5195a79fbe92a3c2a0cdac421960dd
SamanthaT31 <samanthathorng3@gmail.com>
2018-06-08 08:34:12 +0200

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Gestion des rapports de visites</h1><router-outlet></router-outlet>'
+  
+})
+export class AppComponent { 
+
+
+}
